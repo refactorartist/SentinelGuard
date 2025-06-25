5912d6118fe9100743b02427cbad75a30b366483
Khalid Zubair <khalid.zubair@gmail.com>
2025-06-25 10:49:20 +0200

    refactor(services): reorganize service module imports and enhance update method formatting
    - Sort mod.rs
    - Refactor the `update` method in `ProjectAccessScopesService` for better readability by formatting parameters across multiple lines.

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,7 +1,7 @@
 pub mod base;
 pub mod environment_service;
+pub mod project_access_scopes_service;
+pub mod project_access_service;
 pub mod project_scope_service;
 pub mod project_service;
 pub mod service_account_service;
-pub mod project_access_service;
-pub mod project_access_scopes_service;
