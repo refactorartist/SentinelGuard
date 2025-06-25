6fb3b249f167acd1754d70a453b0fdc4b077447a
Khalid Zubair <khalid.zubair@gmail.com>
2025-06-25 11:35:03 +0200

    feat(routes): add project access routes for managing project access operations
    - Introduce `project_access_route` module to handle CRUD operations for project access.
    - Implement endpoints for creating, retrieving, updating, and deleting project access entries.
    - Register the new route in `mod.rs` for accessibility within the application.

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,4 +1,5 @@
 pub mod environment_route;
+pub mod project_access_route;
 pub mod project_route;
 pub mod project_scope_route;
 pub mod service_account_route;
---
+++ src/routes/project_access_route.rs
@@ -0,0 +1,143 @@
+use crate::models::pagination::Pagination;
+use crate::models::project_access::{
+    ProjectAccessFilter, ProjectAccessResponse, ProjectAccessSortOrder,
+    ProjectAccessSortableFields, ProjectAccessUpdatePayload,
+};
+use crate::models::sort::SortOrder;
+use crate::services::project_access_service::ProjectAccessService;
+use crate::{models::project_access::ProjectAccessCreatePayload, services::base::Service};
+use actix_web::{Error, HttpResponse, web};
+use uuid::Uuid;
+
+#[utoipa::path(
+    post,
+    path = "/projects/{project_id}/access",
+    tag = "Projects",
+    request_body = ProjectAccessCreatePayload,
+    responses(
+        (status = 201, description = "Project access created", body = ProjectAccessResponse),
+        (status = 400, description = "Invalid request", body = String),
+    ),
+)]
+pub async fn post(
+    service: web::Data<ProjectAccessService>,
+    payload: web::Json<ProjectAccessCreatePayload>,
+) -> Result<HttpResponse, Error> {
+    let project_access = service
+        .create(payload.into_inner())
+        .await
+        .map_err(actix_web::error::ErrorBadRequest)?;
+    Ok(HttpResponse::Created().json(project_access))
+}
+
+#[utoipa::path(
+    get,
+    path = "/projects/{project_id}/access/{id}",
+    tag = "Projects",
+    responses(
+        (status = 200, description = "Project access retrieved", body = ProjectAccessResponse), 
+        (status = 400, description = "Invalid request", body = String),
+    ),
+)]
+pub async fn get(
+    service: web::Data<ProjectAccessService>,
+    id: web::Path<Uuid>,
+) -> Result<HttpResponse, Error> {
+    let project_access = service
+        .read(id.into_inner())
+        .await
+        .map_err(actix_web::error::ErrorBadRequest)?;
+    Ok(HttpResponse::Ok().json(project_access))
+}
+
+#[utoipa::path(
+    patch,
+    path = "/projects/{project_id}/access/{id}",
+    tag = "Projects",
+    request_body = ProjectAccessUpdatePayload,
+    responses(
+        (status = 200, description = "Project access updated", body = ProjectAccessResponse),
+        (status = 400, description = "Invalid request", body = String),
+    ),
+)]
+pub async fn patch(
+    service: web::Data<ProjectAccessService>,
+    id: web::Path<Uuid>,
+    payload: web::Json<ProjectAccessUpdatePayload>,
+) -> Result<HttpResponse, Error> {
+    let project_access = service
+        .update(id.into_inner(), payload.into_inner())
+        .await
+        .map_err(actix_web::error::ErrorBadRequest)?;
+    Ok(HttpResponse::Ok().json(project_access))
+}
+
+#[utoipa::path(
+    delete,
+    path = "/projects/{project_id}/access/{id}",
+    tag = "Projects",
+    responses(
+        (status = 200, description = "Project access deleted", body = ProjectAccessResponse),
+        (status = 400, description = "Invalid request", body = String),
+    ),
+)]
+pub async fn delete(
+    service: web::Data<ProjectAccessService>,
+    id: web::Path<Uuid>,
+) -> Result<HttpResponse, Error> {
+    let project_access = service
+        .delete(id.into_inner())
+        .await
+        .map_err(actix_web::error::ErrorBadRequest)?;
+    Ok(HttpResponse::Ok().json(project_access))
+}
+
+#[utoipa::path(
+    get,
+    path = "/projects/{project_id}/access",
+    tag = "Projects",
+    responses(
+            (status = 200, description = "Project Access updated", body = ProjectAccessResponse),
+            (status = 400, description = "Invalid request", body = String),
+            (status = 404, description = "Project Access not found", body = String),
+            (status = 409, description = "Project Id, Service Account Id and Environment Id combination already exists", body = String),
+            (status = 500, description = "Internal server error", body = String),
+    ),
+)]
+pub async fn list(
+    service: web::Data<ProjectAccessService>,
+    filter: web::Query<ProjectAccessFilter>,
+    pagination: web::Query<Pagination>,
+) -> Result<HttpResponse, Error> {
+    let sort = vec![ProjectAccessSortOrder {
+        field: ProjectAccessSortableFields::Id,
+        order: SortOrder::Desc,
+    }];
+
+    let project_access = service
+        .find(
+            filter.into_inner(),
+            Some(sort),
+            Some(pagination.into_inner()),
+        )
+        .await
+        .map_err(actix_web::error::ErrorBadRequest)?;
+    Ok(HttpResponse::Ok().json(project_access))
+}
+
+pub fn configure_routes(config: &mut actix_web::web::ServiceConfig) {
+    config.service(
+        web::scope("/projects")
+            .service(
+                actix_web::web::resource("")
+                    .route(actix_web::web::post().to(post))
+                    .route(actix_web::web::get().to(list)),
+            )
+            .service(
+                actix_web::web::resource("/{id}")
+                    .route(actix_web::web::get().to(get))
+                    .route(actix_web::web::patch().to(patch))
+                    .route(actix_web::web::delete().to(delete)),
+            ),
+    );
+}
